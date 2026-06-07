9a38be0f7c5719c062608783fa1f676c8c119f0a
Rimon Ahmed Topu <ratopu230@gmail.com>
2026-06-07 13:34:19 +0600

    indexphpadedFor

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en-us">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <title>Integrate Ludo Game API</title>
+    <link rel="stylesheet" href="TemplateData/style.css">
+  </head>
+  <body>
+    <div id="unity-container">
+      <h1>Integrate Ludo Game API</h1>
+      <p>
+        If you want to integrate Ludo game API to your website,
+        contact <a href="https://topuvai.com/">topuvai.com</a>.
+      </p>
+    </div>
+  </body>
+</html>
